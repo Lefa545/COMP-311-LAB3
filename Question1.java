bc562d00031a1834cf71ecb83f5c91a961d90e47
Lefa Ramonameng <ramonamenglefa@gmail.com>
2026-09-13 17:05:49 +0200

    While loop that counts down

---
+++ Question1.java
@@ -0,0 +1,16 @@
+public class Question1{
+
+    // main method
+    public static void main(String[] args){
+        int count = 10;
+
+        // writes a while loop that counts down from 10-1.
+        while(count > 0){
+            System.out.println(count);
+            count--;
+        }
+        //prints liftoff! after the loop finishes counting.
+        System.out.println("Liftoff!");
+    }
+
+}
